bd9e2f2870718b17ab5977716d0dfa296c3b046b
tomasz <tomsteckiewicz@gmail.com>
2025-10-18 12:38:21 +0200

    simple search

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.db.models import Q
 from .models import Recipe, Category, Tag
 
 class RecipeListView(ListView):
     model = Recipe
     template_name = 'recipes/recipe_list.html'
     context_object_name = 'recipes'
+
+    def get_queryset(self):
+        queryset = Recipe.objects.all().order_by('-created_at')
+        search_query = self.request.GET.get('q', '')
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query)|
+                Q(ingredients__icontains=search_query) |
+                Q(description__icontains=search_query)
+                )
+        return queryset
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_query'] = self.request.GET.get('q', '')
+        return context
+    
 
 class RecipeDetailView(DetailView):
     model = Recipe
